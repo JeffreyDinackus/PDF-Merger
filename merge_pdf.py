b43d17f0498d23b4099f4f46ac53695afa427c92
JeffreyDinackus <jeffreydinackus@proton.me>
2023-07-27 21:15:25 -0400

    project complete, now uses PyMuPDF

--- merge_pdf.py
+++ merge_pdf.py
@@ -2,37 +2,39 @@
 
 # Importing the required modules
 import time
-from pypdf import PdfWriter
+import fitz  # PyMuPDF
 
-def merge_pdfs(file1, file2, output_file):
-    merger = PdfWriter()
+def merge_pdfs(input_paths, output_path):
+    pdf_writer = fitz.open()
 
-    pdf_merger = PyPDF2.PdfMerger()
+    for path in input_paths:
+        pdf_reader = fitz.open(path)
+        pdf_writer.insert_pdf(pdf_reader)
+        pdf_reader.close()
 
-    for pdf in []:
-        merger.append(pdf)
-
-    merger.write("merged-pdf.pdf")
-    merger.close()
+    pdf_writer.save(output_path)
+    pdf_writer.close()
 
 def main():
-  print("Welcome to PDF Merger!")
-  time.sleep(1)
-  print("Make sure that the PDFs you want to merge are in the same folder as this program.")
-  time.sleep(1)
-  print("Please enter the names of the PDFs you want to merge, in the order you want them to be merged.")
-  print("For example, if you want to merge 'file1.pdf' and 'file2.pdf' with 'file1.pdf' first, enter 'file1.pdf file2.pdf'.")
-  time.sleep(1)
+    print("Welcome to PDF Merger!")
+    time.sleep(1)
+    print("Make sure that the PDFs you want to merge are in the same folder as this program.")
+    time.sleep(1)
+    print("Please enter the names of the PDFs you want to merge, in the order you want them to be merged.")
+    print("For example, if you want to merge 'file1.pdf' and 'file2.pdf' with 'file1.pdf' first, enter 'file1.pdf file2.pdf'.")
+    time.sleep(1)
 
-  fiel1 = input("Enter the name of the file you want to go first in the merged PDF: ")
-  file2 = input("Enter the name of the file you want to go second in the merged PDF: ")
+    file1 = input("Enter the name of the first PDF: ")
+    file2 = input("Enter the name of the second PDF: ")
+    input_files = [file1, file2]
+    output_file = "merged_output.pdf"
 
-  merge_pdfs(fiel1, file2, output_file="merged.pdf")
-  print("Merged PDF saved as 'merged.pdf'.")
-  time.sleep(1)
-  print("Thank you for using PDF Merger!")
-  print("Exiting...")
-  exit()
+    merge_pdfs(input_files, output_file)
+    print("Merged PDF saved as 'merged.pdf'.")
+    time.sleep(1)
+    print("Thank you for using PDF Merger!")
+    print("Exiting...")
+    exit()
 
 if __name__ == "__main__":
     main()
